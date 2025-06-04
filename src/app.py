3b528c13360d1d310d509e05d6dd241b406be442
William Ironmore <86080749+willGatos@users.noreply.github.com>
2025-06-04 14:02:15 -0400

    try2

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello World!"  # Simple response for testing
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
