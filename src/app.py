f8585b529ac6911eadd3fdcc506416c9ccc57f0f
William Ironmore <86080749+willGatos@users.noreply.github.com>
2025-06-04 13:55:04 -0400

    try2

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
